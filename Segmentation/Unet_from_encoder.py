eaa8bbc2f18bf0a3ac8caafb66134c38a0a47751
pswapnesh <swapnesh.off@gmail.com>
2021-01-15 18:38:22 +0100

    test example

--- Segmentation/Unet_from_encoder.py
+++ Segmentation/Unet_from_encoder.py
@@ -13,8 +13,10 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 size = 128
 inp = tf.keras.layers.Input(shape = (size,size,3))
 encoder_model = ResNet50(input_tensor=inp,weights='imagenet',include_top = False)
-
-unet = UNET(128,3,encoder_model)
+for l in encoder_model.layers:
+    l.trainable = False
+    
+unet = UNET(128,3,encoder_model,filter_start=16)
 encoder_model = []
 
 """
