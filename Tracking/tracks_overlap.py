e3c9ec305498932b32cbaa30c28340fc344fee99
pswapnesh <50742608+pswapnesh@users.noreply.github.com>
2023-04-12 14:32:28 +0200

    parallel overlap

--- Tracking/tracks_overlap.py
+++ Tracking/tracks_overlap.py
@@ -22,31 +22,33 @@ from joblib import Parallel, delayed
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import min_weight_full_bipartite_matching
 from scipy.sparse.csgraph import maximum_bipartite_matching
-
+from skimage.morphology import skeletonize
 
 # Example usage
 """
 from glob import glob
-from tracks_overlap_parallel import *
-from skimage.segmentation import clear_border
-from skimage.morphology import remove_small_objects
+from tracks_overlap import *
 
-folder = "../inputs/images_pour_swap/"
-N = 30
-flist = [folder + str(i) + '.tif' for i in range(1,N+1)]
-#flist = sorted(glob(folder + '*.tif'))[:10]
+folder = "../inputs/segmented_cropped_ws/"
+flist = sorted(glob(folder + '*.tif'))
 def label_reader(ii):
-    labeled1 = imread(flist[ii]).astype(int) 
-    labeled1 = remove_small_objects(labeled1,256)
+    yy = imread(flist[ii])/255.0    
+    yy = remove_small_objects(yy>0.9,256)
+    labeled1 = label(yy)
     return labeled1
 
-tr = Tracker(flist,label_reader=label_reader,n_jobs=4)
-tr.min_overlap_percentage = 5/100 # 5 percent
+tr = Tracker(flist,label_reader=label_reader)
 tr.start()
-tr.save('test.csv')
-tr.tracks.head()
-
+tr.save('outputs/tracks.csv')
 """
+def center_of_mass(region,intensity):
+    skel = skeletonize(region)
+    rr,cc = np.where(skel)
+    r0,c0 = np.mean(rr),np.mean(cc)
+    distances = (rr-r0)**2 + (cc-c0)**2
+    idx = np.argmin(distances)
+    r0,c0 = rr[idx],cc[idx]
+    return r0,c0
 
 def overlap_histogram(region,intensity):
     threshold = 0.02
@@ -81,22 +83,6 @@ def compute_overlap_cost(props1,props2):
             cost_matrix[i,idx] = -np.log(h1)
     #cost_matrix = cost_matrix- np.min(cost_matrix)         
     return cost_matrix,has_overlaps
-
-# def compute_overlap_cost(props1,props2):
-    
-#     labels1 = props1['label'].values
-#     labels2 = props2['label'].values
-#     #has_overlaps = [False if (len(l)==1) & (l[0]==0) else True for i,(l,h) in enumerate(props2['overlap'])]
-#     has_overlaps = [False if (l[0]==0) else True for i,(l,h) in enumerate(props2['overlap'].values)]
-#     has_overlaps_indices = np.where(has_overlaps)[0]
-#     cost_matrix = np.zeros((len(props1),len(has_overlaps_indices)))
-
-#     for i,(l,h) in enumerate(props2.loc[has_overlaps_indices,'overlap'].values):
-#         for l1,h1 in zip(l,h):
-#             idx = np.where(labels1==l1)[0]
-#             cost_matrix[idx,i] = h1
-#     cost_matrix = np.exp(-cost_matrix/2)
-#     return cost_matrix,has_overlaps
 
 
 
@@ -134,11 +120,15 @@ class Tracker:
     def compute_overlaps(self,t):
         l1 = self.label_reader(t-1)
         l2 = self.label_reader(t)
-        properties = pd.DataFrame(regionprops_table(l2,intensity_image = l1,properties=['label','centroid'],extra_properties = (overlap_histogram,)))
+        #properties = pd.DataFrame(regionprops_table(l2,intensity_image = l1,properties=['label','centroid'],extra_properties = (overlap_histogram,)))
+        properties = pd.DataFrame(regionprops_table(l2,intensity_image = l1,properties=['label','bbox'],extra_properties = (center_of_mass,overlap_histogram)))
+        
+        properties['center_of_mass-0'] += properties['bbox-0']
+        properties['center_of_mass-1'] += properties['bbox-1']
         
         tmp_tracks = pd.DataFrame(columns = ['t','y','x','label'])
-        tmp_tracks['y'] = properties['centroid-0']
-        tmp_tracks['x'] = properties['centroid-1']
+        tmp_tracks['y'] = properties['center_of_mass-0']
+        tmp_tracks['x'] = properties['center_of_mass-1']
         tmp_tracks['overlap'] = properties['overlap_histogram']
         tmp_tracks['label'] = properties['label'].values.astype(int)
         tmp_tracks['t'] = t
@@ -174,10 +164,15 @@ class Tracker:
     def start(self):
         # first frame
         l1 = self.label_reader(0)
-        properties = pd.DataFrame(regionprops_table(l1,intensity_image = l1,properties=['label','centroid'],extra_properties = (overlap_histogram,)))
+        properties = pd.DataFrame(regionprops_table(l1,intensity_image = l1,properties=['label','bbox'],extra_properties = (center_of_mass,overlap_histogram,)))
+        #print(properties.columns)
+        properties['center_of_mass-0'] += properties['bbox-0']
+        properties['center_of_mass-1'] += properties['bbox-1']
+
+
         df0 = pd.DataFrame(columns = ['t','y','x','label'])
-        df0['y'] = properties['centroid-0']
-        df0['x'] = properties['centroid-1']
+        df0['y'] = properties['center_of_mass-0']
+        df0['x'] = properties['center_of_mass-1']
         df0['label'] = properties['label'].values.astype(int)
         df0['overlap'] = properties['overlap_histogram']
         df0['distances'] = 0
